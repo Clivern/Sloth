03d6645bd8b9bb9aca57f3b4b16ee5eee2f58665
Clivern <hello@clivern.com>
2022-12-31 23:54:50 +0100

    init

--- core/check/cert.go
+++ core/check/cert.go
@@ -3,3 +3,9 @@
 // license that can be found in the LICENSE file.
 
 package check
+
+// CertCheck health check
+type CertCheck struct {
+	ID   string
+	Type string
+}
--- core/check/http.go
+++ core/check/http.go
@@ -3,3 +3,9 @@
 // license that can be found in the LICENSE file.
 
 package check
+
+// HttpCheck health check
+type HttpCheck struct {
+	ID   string
+	Type string
+}
